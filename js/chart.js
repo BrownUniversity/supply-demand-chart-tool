20dad44cc2eb6a31960d5b8a639b2d6bd04013e6
kellyegan <kelly.egan@gmail.com>
2019-04-13 21:21:38 -0400

    Basic line drawing on the canvas with paper.js.

--- js/chart.js
+++ js/chart.js
@@ -1,1 +1,24 @@
-console.log("Hello world")
+var style = {
+	strokeColor: '#E4141B',
+	strokeWidth: 8,
+	strokeCap: 'round'	
+};
+
+var lines = [];
+
+function onMouseDown(event) {
+	var line = new Path(style);
+	line.add(event.point);
+	line.add(event.point);
+	lines.push(line);
+}
+
+function onMouseDrag(event) {
+	var line = lines[lines.length - 1];
+	line.lastSegment.point = event.point;
+}
+
+function onMouseUp(event) {
+	console.log("There are " + lines.length + " lines.");
+}
+
