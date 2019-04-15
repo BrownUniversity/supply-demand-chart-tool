94edcceae185b289c49a1e344e79ebdfd77df31c
kellyegan <kelly.egan@gmail.com>
2019-04-15 10:04:47 -0400

    Add axis labels, setItemStyle().

--- js/chart.js
+++ js/chart.js
@@ -1,11 +1,4 @@
-// Chart settings
-var types = ["Supply", "Demand"];
-var numberOfLinesPerType = 2;
 
-var chartPosition = new Point(0, 0);
-var chartSize = new Size(400, 400);
-var chartBoundries = new Rectangle( chartPosition, chartSize);
-chartBoundries.center = view.center;
 
 // Colors
 var blues = [
@@ -20,8 +13,52 @@ var oranges = [
 	"#fdbe85",
 ];
 
+// Chart settings
+var types = ["Supply", "Demand"];
+var numberOfLinesPerType = 2;
+
+var chartPosition = new Point(0, 0);
+var chartSize = new Size(380, 380);
+var chartBoundries = new Rectangle( chartPosition, chartSize);
+chartBoundries.center = view.center;
+
+var xAxisLabelText = "Price";
+var yAxisLabelText = "Quantity";
+
+//Supply and demand lines of chart 
+var chartLines = [
+	{
+		label: "Supply 1",
+		type: "supply",
+		start: 0.2,
+		end: 0.7,
+		color: oranges[0]
+	},
+	{
+		label: "Supply 2",
+		type: "supply",
+		start: 0.3,
+		end: 0.8,
+		color: oranges[1]
+	},
+	{
+		label: "Demand 1",
+		type: "demand",
+		start: 0.7,
+		end: 0.2,
+		color: blues[0]
+	},
+	{
+		label: "Demand 2",
+		type: "demand",
+		start: 0.8,
+		end: 0.3,
+		color: blues[1]
+	}
+];
+
 // Styles
-var chartLineWidth = 7;
+var chartLineWidth = 6;
 var axisLineWidth = 1;
 var intersectionLineWidth = 2;
 var chartLineStrokeCap = "round";
@@ -43,16 +80,99 @@ var axisLineStyles = {
 	strokeWidth: axisLineWidth,
 }
 
+var axisLabelStyles = {
+	color: "black",
+	fontFamily: "'Roboto', 'sans-serif'",
+	fontSize: 24
+
+}
+
+drawAxis( chartBoundries );
+drawAxisLabels( xAxisLabelText, yAxisLabelText, chartBoundries );
+drawChartLines( chartLines, chartBoundries );
+
 /**
  * Draw a charts axis given its boundries
  * @param {*} chartBoundries 
  */
 function drawAxis( chartBoundries ) {
 	var leftAxis = new Path.Line(chartBoundries.topLeft, chartBoundries.bottomLeft);
-	leftAxis.strokeColor = axisLineStyles.strokeColor;
+	setItemStyle(leftAxis, axisLineStyles);
 
 	var bottomAxis = new Path.Line(chartBoundries.bottomLeft, chartBoundries.bottomRight);
-	bottomAxis.strokeColor = axisLineStyles.strokeColor;
+	setItemStyle(bottomAxis, axisLineStyles);
 }
 
-drawAxis( chartBoundries );
+function drawAxisLabels( xAxisLabelText, yAxisLabelText, chartBoundries ){
+	var xLabelPosition = new Point(chartBoundries.bottomCenter);
+	xLabelPosition.y += 30;
+	var xAxisLabel = new PointText( xLabelPosition );
+	setItemStyle(xAxisLabel, axisLabelStyles);
+	xAxisLabel.justification = "center";
+	xAxisLabel.content = xAxisLabelText;
+	
+
+	var yLabelPosition = new Point(chartBoundries.leftCenter);
+	yLabelPosition.x -= 10;
+	var yAxisLabel = new PointText( yLabelPosition );
+	setItemStyle(yAxisLabel, axisLabelStyles);
+	yAxisLabel.justification = "right";
+	yAxisLabel.content = yAxisLabelText;	
+}
+
+/**
+ * Apply styles in a given object to an item
+ * @param {*} item 
+ * @param {*} itemStyles 
+ */
+function setItemStyle( item, itemStyles ) {
+	for( style in itemStyles ){
+		if(itemStyles.hasOwnProperty(style)) {
+			item[style] = itemStyles[style];
+		}
+	}
+}
+
+/**
+ * Create the lines of the chart
+ * @param {*} chartLines 
+ * @param {*} chartBoundries 
+ */
+function drawChartLines( chartLines, chartBoundries ){
+	for(var i = 0; i < chartLines.length; i++) {
+		currentLine = chartLines[i];
+
+		var startPoint = getChartPosition(0, currentLine.start, chartBoundries);
+		var endPoint = getChartPosition(1.0, currentLine.end, chartBoundries);
+
+		var linePath = new Path.Line(startPoint, endPoint);
+		linePath.strokeWidth = chartLineWidth;
+		linePath.strokeColor = currentLine.color;
+		linePath.strokeCap = chartLineStrokeCap;
+	}
+}
+
+/**
+ * Get position of lines on chart from x and y values between 0 and 1
+ * @param {*} x 
+ * @param {*} y 
+ * @param {*} chartBoundries 
+ */
+function getChartPosition( x, y, chartBoundries ) {
+	var xPos = chartBoundries.left + chartBoundries.width * x;
+	var yPos = chartBoundries.top + chartBoundries.height - chartBoundries.height * y;
+
+	return new Point(xPos, yPos);
+}
+
+function getUnitPosition( point, chartBoundries ) {
+	var x = (point.x - chartBoundries.left) / chartBoundries.width;
+	var y = (chartBoundries.top + chartBoundries.height - point.y) / chartBoundries.height;
+	return new Point(x, y);
+}
+
+function xScale( lineValue ){
+	var chartPosition = 0;
+
+	return chartPosition;
+} 
